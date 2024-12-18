ddaf2b18c0ce92f46d4a6efa291c4a39ddf4a53b
candraKriswinarto <ckriswinarto@gmail.com>
2024-12-18 13:44:32 +0800

    initial

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+// import Invoice from "./components/invoice";
+import Invoice from "./components/invoice";
+import Invoice2 from "./components/invoice/invoice";
+
+function App() {
+  return (
+    <div>
+      <Invoice />
+      <br />
+      <Invoice2 />
+    </div>
+  );
+}
+
+export default App;
